c7cec5f6a3cc50f65de1158e9c2fe76f8b15bcc5
kvn_wrk <41454606+enigmaedu@users.noreply.github.com>
2020-12-17 19:40:32 -0800

    initial commit

---
+++ Intro/BasicConcepts/MethodDefaultParam.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace BasicConcepts
+{
+    class MethodParamArgs
+    {
+        static void MyMethod(string fname)
+        {
+            Console.WriteLine(fname + " Refsnes");
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod("Kevin");
+            MyMethod("Pam");
+            MyMethod("Anja");
+        }
+    }
+}
---
+++ Intro/BasicConcepts/MethodIntro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BasicConcepts
+
+{
+    class MethodIntro
+    {
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed!");
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod();
+            MyMethod();
+            MyMethod();
+        }
+    }
+}
