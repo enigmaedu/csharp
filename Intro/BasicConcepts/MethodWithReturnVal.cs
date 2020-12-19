8ff71376df9ef61adbcc498a55980c2e295abd85
enigmaedu <41454606+enigmaedu@users.noreply.github.com>
2020-12-19 08:41:18 -0800

    new method added

---
+++ Intro/BasicConcepts/MethodWithReturnVal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BasicConcepts
+{
+    class MethodWithReturnVal
+    {
+        static int MyMethod (int x)
+        {
+            Console.WriteLine("This method retruns a value!!!");
+            return 5 + x;
+        }
+        static void Main(string [] args)
+        {
+            Console.WriteLine("This is the entry point of the program!");
+            Console.WriteLine(MyMethod(4));
+
+        }
+
+    }
+}
