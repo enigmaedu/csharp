3d36e0ce4f25f95d91a6a09ddb79700895645a90
enigmaedu <41454606+enigmaedu@users.noreply.github.com>
2020-12-20 18:12:45 -0800

    named method overloading added

---
+++ Intro/BasicConcepts/MethodOverloading.cs
@@ -0,0 +1,30 @@
+﻿/*Multiple methods can have the same name with different parameters
+Multiple methods can have the same name as long as the number 
+and/or type of parameters are different.
+*/
+
+using System;
+
+namespace BasicConcepts
+{
+    class MethodOverloading
+    {
+        static int PlusMethod(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+
+        static double PlusMethod(double num1,double num2)
+        {
+            return num1 + num2;
+        }
+        static void Main (string[] args)
+        {
+            Console.WriteLine("This is the entry point of the program!");
+            int int_numsum = PlusMethod(3, 4);
+            Console.WriteLine("sum of int numbers: " + int_numsum);
+            double double_numsum = PlusMethod(3.1, 4.1);
+            Console.WriteLine("sum of double numbers: " + double_numsum);
+        }
+    }
+}
---
+++ Intro/BasicConcepts/MethodWithNamedArguments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BasicConcepts
+{
+    class MethodWithNamedArguments
+    {
+        static string my_method(string fname, string lname, string mname = "N/A")
+            //Optional arguments should be placed at the end 
+        {
+        return  fname + " " + mname + " " + lname;
+        }
+        
+        static void Main(string[] args)
+        {
+        Console.WriteLine("This is the entry point of the program");
+        string fullname = my_method(fname:"John",lname:"Smith");//here mname takes the default value
+        Console.WriteLine(fullname);
+        string fullname1 = my_method(fname: "Mathew", mname:"Aus",lname: "Hayden");
+        Console.WriteLine(fullname1);
+        }
+    }
+}
